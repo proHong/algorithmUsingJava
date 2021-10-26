79f76d549e8992cd8f7d47b5cf08a8da7c840cab
proHong <tjdgus0454@gmail.com>
2021-10-26 16:43:55 +0900

    [홍성현][boj 1000, 10001]

---
+++ BOJ/boj1000.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/1000
+public class boj1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer a = sc.nextInt();
+        Integer b = sc.nextInt();
+        System.out.println(a+b);
+        sc.close();
+    }
+}
